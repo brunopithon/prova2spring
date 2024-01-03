34cd578770180e89f4c6d9a6cb24f48d8a869edf
Bruno Pithon <brunopithon@velty.com.br>
2024-01-03 08:25:34 -0300

    Implementação da domain de consulta, medico e paciente

---
+++ src/main/java/br/edu/ifrn/saude/domain/medico/Medico.java
@@ -0,0 +1,35 @@
+package br.edu.ifrn.saude.domain.paciente;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "medico") 
+@Table(name = "medico")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column (name = "especialidade")
+    private String especialidade;
+
+    @Column (name = "crm")
+    private String crm;
+}
