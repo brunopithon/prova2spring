23a35fd691fbde7c73287e7c2943b9dc4b7acc85
Bruno Pithon <brunopithon@velty.com.br>
2024-01-03 09:39:45 -0300

    Criação da regra de negócio

--- src/main/java/br/edu/ifrn/saude/controller/PacienteController.java
+++ src/main/java/br/edu/ifrn/saude/controller/PacienteController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,6 +44,10 @@ public class PacienteController {
     @Transactional
     public ResponseEntity<Object> cadastrar(@RequestBody @Valid Paciente paciente,
             UriComponentsBuilder uriBuilder) {
+        if (repository.existsByCpf(paciente.getCpf())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body("CPF já cadastrado. Não é permitido cadastrar pacientes com CPF duplicado.");
+        }    
         Paciente pacienteLocal = repository.save(paciente);
         var uri = uriBuilder.path("/paciente/{id}").buildAndExpand(pacienteLocal.getId()).toUri();
         return ResponseEntity.created(uri).build();
--- src/main/java/br/edu/ifrn/saude/repository/PacienteRepository.java
+++ src/main/java/br/edu/ifrn/saude/repository/PacienteRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.edu.ifrn.saude.domain.paciente.Paciente;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long>{
-
+    boolean existsByCpf(String cpf);
 }
